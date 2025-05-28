95429db0a520dabcb1223b998b1ef81c700a5d68
idansluprisma <132197196+idansluprisma@users.noreply.github.com>
2025-05-28 22:12:49 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -306,3 +306,8 @@ output "public_subnet2" {
   description = "The ID of the Public subnet"
   value       = aws_subnet.web_subnet2.id
 }
+
+module "s3-bucket" {
+  source  = "cloudposse/s3-bucket/aws"
+  version = "2.0.0"
+}
